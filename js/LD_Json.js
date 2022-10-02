99b8e9b06619b1f576081239bd729d4eda9aa64c
Eymeric Le Bohec <lebohec.eymeric@gmail.com>
2022-10-02 19:28:45 +0200

    supprimer fichiers js

--- js/LD_Json.js
+++
@@ -1,8 +0,0 @@
-<script type="application/ld+json">
-{
-  "@context": "https://eymeric1992.github.io/Starting-website-2/",
-  "@type": "Person",
-  "name": "La Panthere",
-  "url": "https://eymeric1992.github.io/Starting-website-2/"
-}
-</script>
